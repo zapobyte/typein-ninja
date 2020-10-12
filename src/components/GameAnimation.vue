62aece2688a4fd80cdea63574efa73d756437ffd
victor <>
2020-10-12 23:54:29 +0100

    removed animation

--- src/components/GameAnimation.vue
+++ src/components/GameAnimation.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="animation-container">
-    <ion-phaser
+  <!--   <ion-phaser
       v-bind:game.prop='config'
       v-bind:initialize.prop='initialize'
-      v-if="initialize"/>
+      v-if="initialize"/> -->
       <img src="@/assets/idle.gif"/>
   </div>
 </template>
 
 <script>
-import {config} from '@/functions/gameAnimation';
- 
+/* import {config} from '@/functions/gameAnimation';
+ */ 
 export default {
   name:"GameAnimation",
   data() {
     return {
       initialize: false,
-      config:config
-    };
+/*       config:config
+ */    };
   },
   methods: {
 
