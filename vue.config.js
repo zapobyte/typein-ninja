3e7f57b3d692723315908ca609511a1dc7d7d47d
victor <>
2020-11-06 09:31:00 +0000

    fixed build

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,17 @@ const version = JSON.parse(packageJson).version || 0;
 const webpack = require('webpack');
 module.exports = {
     // enable/disable eslint error messages
-    lintOnSave:false,
+    lintOnSave:true,
     css: {
         loaderOptions: {
-          scss: {
-            additionalData: `
-            @import "@/scss/variables.scss";
-            `
-          },
+            sass: {
+                additionalData: `
+                @import "~@/scss/variables.scss"
+                `
+            },
+            scss: {
+                additionalData: `@import "~@/scss/variables.scss";`
+            },
         }
     },
     configureWebpack: {
@@ -21,10 +24,7 @@ module.exports = {
                     VUE_APP_PACKAGE_VERSION: '"' + version + '"'
                 }
             })
-        ],
-        output: {
-            filename: '[name].[hash].js', // force all files to have the build number thus refresing on each new build
-        },
-    }, 
+        ]
+    }
     //filenameHashing: false
 }
